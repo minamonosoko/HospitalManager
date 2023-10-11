ef7a01e26f868216c5a7916ffd05d07f2ff5a3bd
minamonosoko <>
2023-10-11 20:56:51 +0900

    導入：Laravel, Bootstrap, JetStream

---
+++ portfolio/resources/views/top.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hospital Manager</title>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="alert alert-success" role="alert">
+            Bootstrapでデザイン
+            </div>
+    </body>
+</html>
